f8ee0932be1ed46873275018f6c7b214d08c57ee
Yuii24 <linian2040@gmail.com>
2024-03-17 17:05:39 +0800

    add :handlebars

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const engine = require("express-handlebars")
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
+// app.engine("handlebars", engine());
+// app.set("view engine", "handlebars");
+// app.set("views", "./views");
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect("/restaurants")
